2550036850120a95b94eb050d89a04b48ef858ed
ksavukov <savukovkirill12@gmail.com>
2018-04-04 14:23:37 +0300

    Выбор операции: 1- калькулятор, 2- массивы

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -9,36 +9,55 @@ public class Base {
 
         /** @see Scanner */
         Scanner scanner = new Scanner(System.in);
-
-        /** Ввод первого числа с плавающей точкой */
-        System.out.println("Введите первое число");
-        float f = scanner.nextFloat();
-
-        /** Ввод второго числа с плавающей точкой */
-        System.out.println("Введите второе число");
-        float fs = scanner.nextFloat();
-
-        /** Выбор операции */
-        System.out.println("Выберите операцию: ");
-        System.out.println("1 - Сложение");
-        System.out.println("2 - Вычитание");
-        System.out.println("3 - Деление");
-        System.out.println("4 - Умножение");
-        System.out.println("5 - Выход");
-        int operation = scanner.nextInt();
-
-        /** Вывод оперции */
-        if (operation == 1) {
-            System.out.printf("%.4f", f + fs);
-        } else if (operation == 2) {
-            System.out.printf("%.4f", f - fs);
-        } else if (operation == 3) {
-            System.out.printf("%.4f", f / fs);
-        } else if (operation == 4) {
-            System.out.printf("%.4f", f * fs);
-        }
-        else {
-            System.exit(0);
+        System.out.println("Выберите задание");
+        System.out.println("1-калькулятор");
+        System.out.println("2-поиск максимального слова в массиве");
+        int task = scanner.nextInt();
+        if (task == 1) {
+          Base.calc();
+        }else if (task == 2){
+           Base.massived();
         }
     }
-}
+        public static void calc() {
+            //Ввод первого числа с плавающей точкой
+            Scanner scanner = new Scanner(System.in);
+             System.out.println("Введите первое число");
+             float f = scanner.nextFloat();
+
+            // Ввод второго числа с плавающей точкой
+             System.out.println("Введите второе число");
+             float fs = scanner.nextFloat();
+
+            //Выбор операции
+             System.out.println("Выберите операцию: ");
+             System.out.println("1 - Сложение");
+             System.out.println("2 - Вычитание");
+             System.out.println("3 - Деление");
+             System.out.println("4 - Умножение");
+             System.out.println("5 - Выход");
+             int operation = scanner.nextInt();
+
+            // Вывод оперции
+             if (operation == 1) {
+             System.out.printf("%.4f", f + fs);
+             } else if (operation == 2) {
+             System.out.printf("%.4f", f - fs);
+             } else if (operation == 3) {
+             System.out.printf("%.4f", f / fs);
+             } else if (operation == 4) {
+             System.out.printf("%.4f", f * fs);
+             }
+             else {
+             System.exit(0);
+
+             }
+
+        }
+
+        public static void massived(){
+
+        }
+
+    }
+
